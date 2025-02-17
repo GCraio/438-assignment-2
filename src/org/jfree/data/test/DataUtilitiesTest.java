6ad04a34965b3ecc21f7f2216ba3fcce7951bc32
GCraio <gabriel.craiovan@ucalgary.ca>
2025-02-17 15:55:26 -0700

    Initial commit

---
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -0,0 +1,16 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.junit.Test;
+
+public class DataUtilitiesTest extends DataUtilities {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+//		test change 1
+	}
+
+}
