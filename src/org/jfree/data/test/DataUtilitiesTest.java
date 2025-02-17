317b77c631f7d2127e148d942e97ee16aa2c0b3f
GCraio <gabriel.craiovan@ucalgary.ca>
2025-02-17 16:56:32 -0700

    created junit test code for 3 methods

--- src/org/jfree/data/test/DataUtilitiesTest.java
+++ src/org/jfree/data/test/DataUtilitiesTest.java
@@ -1,16 +1,27 @@
 package org.jfree.data.test;
-
 import static org.junit.Assert.*;
-
 import org.jfree.data.DataUtilities;
 import org.junit.Test;
+import org.jfree.data.Values2D;
+import org.jmock.Mockery;
+import org.jmock.Expectations;
 
 public class DataUtilitiesTest extends DataUtilities {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-//		test change 1
-	}
-
+	// Gabriel's implementation
+    @Test
+    public void testCalculateColumnTotal_ValidValues() {
+        Mockery context = new Mockery();
+        Values2D mockData = context.mock(Values2D.class);
+        
+        context.checking(new Expectations() {{
+            allowing(mockData).getRowCount(); will(returnValue(3));
+            allowing(mockData).getValue(0, 0); will(returnValue(2.0));
+            allowing(mockData).getValue(1, 0); will(returnValue(3.5));
+            allowing(mockData).getValue(2, 0); will(returnValue(4.5));
+        }});
+        
+        double result = DataUtilities.calculateColumnTotal(mockData, 0);
+        assertEquals(10.0, result, 0.0001);
+    }
 }
--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -1,31 +1,21 @@
 package org.jfree.data.test;
-
-import static org.junit.Assert.*; import org.jfree.data.Range; import org.junit.*;
+import static org.junit.Assert.*; 
+import org.jfree.data.Range; 
+import org.junit.*;
 
 public class RangeTest {
-    private Range exampleRange;
-    @BeforeClass public static void setUpBeforeClass() throws Exception {
-    	
-    	
-    }
-
-
-    @Before
-    public void setUp() throws Exception { exampleRange = new Range(-1, 1);
-    }
-
-
+	
+	// Gabriel's implementation
     @Test
-    public void centralValueShouldBeZero() {
-        assertEquals("The central value of -1 and 1 should be 0",
-        0, exampleRange.getCentralValue(), .000000001d);
+    public void testGetLength_NormalRange() {
+        Range range = new Range(0, 10);
+        assertEquals(10, range.getLength(), 0.0001);
     }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
+    
+    // Gabriel's implementation
+    @Test
+    public void testContains_Positive() {
+        Range range = new Range(0, 10);
+        assertTrue(range.contains(5));  // Correct assertion for boolean return value
     }
 }
